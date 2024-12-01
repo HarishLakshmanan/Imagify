5e1e735948e391363565d79f37ecf02a424d35fd
=node <harish99427@gmail.com>
2024-12-01 21:45:24 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React, { useContext } from 'react'
 import {Route, Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+
+
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
@@ -7,6 +11,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import { AppContext } from './context/AppContext'
+
 
 const App = () => {
 
@@ -17,6 +22,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen 
     bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       {showLogin && <Login/>}
       <Routes>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,55 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/imagify-assets/assets/assets'
 import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
 
     const [state,setState]=useState('LogIn')
-    const {setShowLogin}=useContext(AppContext)
+    const {setShowLogin,backendUrl,setToken,setUser}=useContext(AppContext)
 
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const onSubmitHandler=async(e)=>{
+      e.preventDefault();
+
+
+      try {
+        if(state === 'Login'){
+          const {data} = await axios.post(backendUrl + '/api/user/login',
+            {email,password})
+
+            if(data.success){
+               setToken(data.token)
+               setUser(data.user)
+               localStorage.getItem('token',data.token)
+               setShowLogin(false)
+            }else{
+             toast.error(data.message) 
+            }
+        }else{
+          const {data} = await axios.post(backendUrl + '/api/user/register',
+            {name,email,password})
+
+            if(data.success){
+               setToken(data.token)
+               setUser(data.user)
+               localStorage.getItem('token',data.token)
+               setShowLogin(false)
+            }else{
+             toast.error(data.message) 
+            }
+        }
+
+
+      } catch (error) {
+        toast.error(error.message)
+      }
+    }
+    
     useEffect(()=>{
       document.body.style.overflow='hidden';
 
@@ -18,7 +61,8 @@ const Login = () => {
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm
     bg-black/30 flex justify-center items-center'>
-      <form className='relative bg-white p-10 rounded-xl text-slate-500'>
+      <form onSubmit={onSubmitHandler} 
+      className='relative bg-white p-10 rounded-xl text-slate-500'>
         <h1 className='text-center text-2xl text-neutral-700 font-medium
         '>{state}</h1>
         <p className='text-sm'>Welcome back! please sign in to continue</p>
@@ -26,21 +70,24 @@ const Login = () => {
         { state !=='LogIn' && <div className='border px-5 py-2 flex items-center gap-2
         rounded-ful mt-5'>
             <img src={assets.profile_icon} alt=""  className='w-5'/>
-            <input type="text" className='outline-none text-sm' 
+            <input onChange={e=>setName(e.target.value)} value={name} 
+            type="text" className='outline-none text-sm' 
             placeholder='Full Name' required />
         </div>}
 
         <div className='border px-6 py-2 flex items-center gap-2
         rounded-ful mt-3'>
             <img src={assets.email_icon} alt="" />
-            <input type="email" className='outline-none text-sm' 
+            <input  onChange={e=>setEmail(e.target.value)} value={email}
+             type="email" className='outline-none text-sm' 
             placeholder='Email Id' required />
         </div>
 
         <div className='border px-6 py-2 flex items-center gap-2
         rounded-ful mt-3'>
             <img src={assets.lock_icon} alt="" />
-            <input type="password" className='outline-none text-sm' 
+            <input  onChange={e=>setPassword(e.target.value)} value={password}
+             type="password" className='outline-none text-sm' 
             placeholder='Password' required />
         </div>
 
